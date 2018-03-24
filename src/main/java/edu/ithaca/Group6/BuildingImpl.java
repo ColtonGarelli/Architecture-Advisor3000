e147676c083b8e4667635f41d5baaa5c768a1bd5
Ben Brown-McMillin <ben.tgms@gmail.com>
2018-03-24 16:15:10 -0400

    Added building interface functions and tests

--- src/main/java/edu/ithaca/Group6/BuildingImpl.java
+++ src/main/java/edu/ithaca/Group6/BuildingImpl.java
@@ -1,5 +1,50 @@
 package edu.ithaca.Group6;
 
-public class BuildingImpl {
+public class BuildingImpl implements Building{
+    //Creating a building in the shape of a rectangular prism
+
+    public void setLength(double newVal) {
+        //set a new length of the building
+    }
+
+    public double getLength() {
+        //get the length of the building
+    }
+
+    public void setWidth(double newVal) {
+        //set a new width for the building
+    }
+
+    public double getWidth() {
+        //get the width of the building
+    }
+
+    public void setHeight(double newVal) {
+        //set a new height for the building
+    }
+
+    public double getHeight() {
+        //get the height of the building
+    }
+
+    public void setExtWallMaterial(String newMaterial) {
+        //set a new material for the external walls
+    }
+
+    public String getExtWallMaterial() {
+        //get the material used to construct the exterior walls
+    }
+
+    public double getSquareFootage() {
+        //get the square footage of the building
+    }
+
+    public double getPerimeter() {
+        //calculate the perimeter of the building
+    }
+
+    public double calcTotalCost() {
+        //Calculate the total cost of the walls
+    }
 
 }
--- src/test/java/edu/ithaca/Group6/Building_Test.java
+++ src/test/java/edu/ithaca/Group6/Building_Test.java
@@ -1,6 +1,72 @@
 package edu.ithaca.Group6;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class Building_Test {
 
+    private BuildingImpl functions = new BuildingImpl();
 
+    @org.junit.jupiter.api.Test
+    void constructorTest(){
+        //test default constructor
+        Building building1 = new Building();
+        //test length
+        double length1 = building1.getLength();
+        assertEquals(0.0, length1, "Innacurate length - Default Constructor");
+        //test width
+        double width1 = building1.getWidth();
+        assertEquals(0.0, width1, "Innacurate width - Default Constructor");
+        //test height
+        double height1 = building1.getHeight();
+        assertEquals(0.0, height1, "Innacurate height - Default Constructor");
+        //test external wall material
+        String material1 = building1.getExtWallMaterial();
+        assertEquals("drywall", material1, "Innacurate Material - Default Constructor");
+
+        Building building2 = new Building(80.0, 50.0, 10.0, "cement");
+        //test length
+        double length2 = building2.getLength();
+        assertEquals(80.0, length2, "Innacurate length");
+        //test width
+        double width2 = building2.getWidth();
+        assertEquals(50.0, width2, "Innacurate width");
+        //test height
+        double height2 = building2.getHeight();
+        assertEquals(10.0, height2, "Innacurate height");
+        //test external wall material
+        String material2 = building2.getExtWallMaterial();
+        assertEquals("cement", material2, "Innacurate Material");
+    }
+
+    void settersTest(){
+        Building building1 = new Building();
+        //test length setter
+        building1.setLength(50.0);
+        assertEquals(50.0, building1.getLength(), "Length not set");
+        //test width setter
+        building1.setWidth(50.0);
+        assertEquals(50.0, building1.getWidth(), "Width not set");
+        //test height setter
+        building1.setHeight(50.0);
+        assertEquals(50.0, building1.getHeight(), "Height not set");
+        //test ext wall material set
+        building1.setExtWallMaterial("cement");
+        assertEquals("cement", building1.getExtWallMaterial(), "Material of Exterior Wall not set");
+    }
+
+    void dimensionTest(){
+        Building building1 = new Building(60.0, 60.0, 12.0, "cement");
+        //test square footage calculation
+        double sqftg1 = building1.getSquareFootage();
+        assertEquals(3600.0, sqftg1, "Innacurate calculation of square footage");
+        //test perimeter calculation
+        double perim1 = building1.getPerimeter();
+        assertEquals(240.0, perim1, "Innaccurate calculation of perimeter");
+    }
+
+    void calcTotalCostTest(){
+        Building building1 = new Building(60.0, 60.0, 12.0, "cement");
+        double totalCost1 = building1.calcTotalCost();
+        assertEquals(15840.0, totalCost1, "Innacurate calculation of total cost");
+    }
 }
