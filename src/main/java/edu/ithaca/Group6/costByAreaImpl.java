3e74e9ee8ebbe19353096220e9f2a338322bb19f
agable74 <agable@ithaca.edu>
2018-03-25 11:56:39 -0400

    Drafted up some tests and impl, 1 hour

--- src/main/java/edu/ithaca/Group6/costByAreaImpl.java
+++ src/main/java/edu/ithaca/Group6/costByAreaImpl.java
@@ -1,4 +1,42 @@
 package edu.ithaca.Group6;
 
-public class costByAreaImpl {
+public class costByAreaImpl implements costByArea{
+    private String materialName;
+    private double costPerSquareMeter;
+
+
+    public costByAreaImpl(String material, double cost){
+        this.materialName = material;
+        this.costPerSquareMeter = cost;
+    }
+
+    @Override
+    public double calcAreaMeter(double areaSquareFeet) {
+        return 0;
+    }
+
+    @Override
+    public double calcCost(double areaSquareMeter) {
+        return 0;
+    }
+
+    @Override
+    public void setMaterialName(String material) {
+        this.materialName = material;
+    }
+
+    @Override
+    public void setCost(double cost) {
+        this.costPerSquareMeter = cost;
+    }
+
+    @Override
+    public String getMaterialName() {
+        return this.materialName;
+    }
+
+    @Override
+    public double getCostPerSquareMeter() {
+        return this.costPerSquareMeter;
+    }
 }
--- src/test/java/edu/ithaca/Group6/costByArea_Test.java
+++ src/test/java/edu/ithaca/Group6/costByArea_Test.java
@@ -1,4 +1,44 @@
 package edu.ithaca.Group6;
 
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
 public class costByArea_Test {
+    private costByArea areaMaterial;
+
+    @BeforeEach
+    void setup(){
+        areaMaterial = new costByAreaImpl("Wood",1.00);
+    }
+
+    @Test
+    void calcAreaMeterTest(){
+
+    }
+
+    @Test
+    void calcCostTest(){
+
+    }
+
+    @Test
+    void setMaterialTest(){
+
+    }
+
+    @Test
+    void getMaterialTest(){
+
+    }
+
+    @Test
+    void setAreaCostTest(){
+
+    }
+
+    @Test
+    void getAreaCostTest(){
+
+    }
+
 }
