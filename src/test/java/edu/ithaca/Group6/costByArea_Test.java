2c600e8ca6babafcf781745fe80e150225f3c277
agable74 <agable@ithaca.edu>
2018-03-25 13:10:22 -0400

    Added tests for accessors and mutators

--- src/test/java/edu/ithaca/Group6/costByArea_Test.java
+++ src/test/java/edu/ithaca/Group6/costByArea_Test.java
@@ -40,22 +40,43 @@ public class costByArea_Test {
 
     @Test
     public void setMaterialTest(){
+        areaMaterial = new costByAreaImpl("Wood",1.00);
+        String emptyString = "";
+        String testMaterial = "Brick";
 
+        areaMaterial.setMaterialName(testMaterial);
+        assertEquals("Brick",areaMaterial.getMaterialName(),"Could not set material");
+
+        assertThrows(NullPointerException.class,()->{areaMaterial.setMaterialName(emptyString);},"Did not throw exception for empty string");
     }
 
     @Test
     public void getMaterialTest(){
-
+        areaMaterial = new costByAreaImpl("Wood",1.00);
+        assertEquals("Wood",areaMaterial.getMaterialName(),"Could not retrieve name");
     }
 
     @Test
     public void setAreaCostTest(){
+        areaMaterial = new costByAreaImpl("Wood",1.00);
+        double positiveCost = 7.00;
+        double zeroCost = 0;
+        double negativeCost = -7.00;
 
+
+        areaMaterial.setCost(positiveCost);
+        assertEquals(positiveCost,areaMaterial.getCostPerSquareMeter(),"Could not change cost to be positive");
+
+        areaMaterial.setCost(zeroCost);
+        assertEquals(0,areaMaterial.getCostPerSquareMeter(),"Could not set to zero cost");
+
+        assertThrows(IndexOutOfBoundsException.class,()->{areaMaterial.setCost(negativeCost);},"Did not throw exception with negative cost");
     }
 
     @Test
     public void getAreaCostTest(){
-
+        areaMaterial = new costByAreaImpl("Wood",1.00);
+        assertEquals(1.00,areaMaterial.getCostPerSquareMeter(),"Could not retrieve cost");
     }
 
 }
