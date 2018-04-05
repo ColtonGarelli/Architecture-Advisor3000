f014b128d705d704c1daaccceaf6bd5cfe299122
agable74 <agable@ithaca.edu>
2018-04-05 09:55:51 -0400

    added some coments to the test

--- src/test/java/edu/ithaca/Group6/UI_Test.java
+++ src/test/java/edu/ithaca/Group6/UI_Test.java
@@ -10,22 +10,30 @@ public class UI_Test {
     @Test
     public void testYesOrNo(){
         testUI = new UIImpl();
+
+        //lowercase and random chars
         String testValidYes = "yes";
         String testValidNo = "no";
         String testInvalidInput = "a7j.3";
         assertEquals(true,testUI.checkYesOrNo(testValidYes),"Did not check that string is valid yes");
         assertEquals(true,testUI.checkYesOrNo(testValidNo),"Did not check that string is valid no");
+
+        //uppercase
         testValidYes = "YES";
         testValidNo = "NO";
         assertEquals(true,testUI.checkYesOrNo(testValidYes),"Did not check that string is valid yes");
         assertEquals(true,testUI.checkYesOrNo(testValidNo),"Did not check that string is valid no");
         assertEquals(false,testUI.checkYesOrNo(testInvalidInput),"Did not check invalid input");
+
+        //uppercase single chars
         testValidYes = "Y";
         testValidNo = "N";
         testInvalidInput = "X";
         assertEquals(true,testUI.checkYesOrNo(testValidYes),"Did not correctly evaluate 'y'");
         assertEquals(true,testUI.checkYesOrNo(testValidNo),"Did not correctly evaluate 'n'");
         assertEquals(false,testUI.checkYesOrNo(testInvalidInput),"Did not correctly evaluate invalid single character");
+
+        //empty string
         String emptyString = "";
         assertEquals(false, testUI.checkYesOrNo(emptyString), "Did not handle empty string");
 
