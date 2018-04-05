2730da2ea489630fe9126e90dc740ae65f4139c0
ColtonGarelli <34031144+ColtonGarelli@users.noreply.github.com>
2018-04-05 09:53:52 -0400

    Update Building_Test.java

--- src/test/java/edu/ithaca/Group6/Building_Test.java
+++ src/test/java/edu/ithaca/Group6/Building_Test.java
@@ -77,41 +77,4 @@ public class Building_Test {
         double totalCost1 = building1.calcTotalCost();
         assertEquals(115.2, totalCost1, "Innacurate calculation of total cost");
     }
-
-    @Test
-    public void setWallDimensionsTest(){
-       assertEquals(true,false,"set wall dimensions test failed");
-    }
-
-    @Test
-    public void getWallDimensionTest(){
-        assertEquals(true,false,"get wall dimensions test failed");
-    }
-
-    @Test
-    public void setWallMaterialTest(){
-        assertEquals(true,false,"set wall material test failed");
-
-    }
-
-    @Test
-    public void getWallMaterialTest(){
-        assertEquals(true,false,"set wall material test failed");
-
-    }
-
-    @Test
-    public void getWallFeatureTest(){
-        assertEquals(true,false,"get wall features test failed");
-    }
-
-    @Test
-    public void addWallFeatureTest(){
-        assertEquals(true,false,"add wall feature test failed");
-    }
-
-    @Test
-    public void removeWallFeatureTest(){
-        assertEquals(true,false,"remove wall features test failed");
-    }
 }
