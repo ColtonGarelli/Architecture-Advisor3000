6b0d8e47afbccc2e45114f3795099bd012d9ec8f
Colton Garelli <Colton Garelli>
2018-04-10 19:40:30 -0400

    dimensions test pass

--- src/main/java/edu/ithaca/Group6/WallImpl.java
+++ src/main/java/edu/ithaca/Group6/WallImpl.java
@@ -11,39 +11,35 @@ public abstract class WallImpl implements Wall{
 
 
 
- class ExternalWall extends WallImpl {
-
+class ExternalWall extends WallImpl {
     private double height;
     private double length;
     private double width;
     private MaterialByArea material = new Brick();
-    private ArrayList <MaterialByUnit> featuresList;
+    private ArrayList<MaterialByUnit> featuresList;
 
-    public ExternalWall(){
+    public ExternalWall() {
         this.featuresList = new ArrayList<MaterialByUnit>();
     }
 
-    public boolean setHeight(double height){
-        if(height > 0) {
+    public boolean setHeight(double height) {
+        if (height > 0) {
             this.height = height;
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
-    public double getHeight(){
+    public double getHeight() {
         return this.height;
     }
 
-    @Override
     public boolean setLength(double length) {
-        if(length > 0){
+        if (length > 0) {
             this.length = length;
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
@@ -57,14 +53,108 @@ public abstract class WallImpl implements Wall{
         if (width > 0) {
             this.width = width;
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
 
-    public double getWidth(){
+    public double getWidth() {
+        return this.width;
+    }
+
+    public boolean setMaterial(MaterialByArea wallMaterial) {
+        this.material = wallMaterial;
+        return true;
+    }
+
+    public MaterialByArea getMaterial() {
+        return this.material;
+    }
+
+
+    public boolean addFeature(MaterialByUnit feature) {
+        this.featuresList.add(feature);
+        return true;
+    }
+
+
+    public MaterialByUnit getFeature(int index) {
+        return this.featuresList.get(index);
+    }
+
+
+    public boolean removeFeature(int index) {
+        if (index > 0 && index < this.featuresList.size() - 1) {
+            this.featuresList.remove(index);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int getFeatureListSize() {
+        return this.featuresList.size();
+    }
+}
+
+class InternalWall extends WallImpl{
+    private double height;
+    private double length;
+    private double width;
+    private MaterialByArea material;
+    private ArrayList<MaterialByUnit> featuresList;
+
+
+    public InternalWall(){
+        this.featuresList = new ArrayList<MaterialByUnit>();
+    }
+
+    public boolean setHeight(double height) {
+        if (height > 0) {
+            this.height = height;
+            return true;
+
+        }
+        else {
+            return false;
+        }
+
+    }
+
+    public double getHeight() {
+            return this.height;
+
+    }
+    public boolean setLength(double length) {
+        if (length > 0) {
+            this.length = length;
+            return true;
+
+        }
+
+        else {
+            return false;
+        }
+    }
+
+        @Override
+    public double getLength() {
+        return this.length;
+    }
+
+    public boolean setThickness(double width) {
+        if (width > 0) {
+            this.width = width;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+    public double getWidth() {
         return this.width;
     }
 
@@ -74,46 +164,34 @@ public abstract class WallImpl implements Wall{
     }
 
 
-    public MaterialByArea getMaterial(){
+    public MaterialByArea getMaterial() {
         return this.material;
     }
 
 
-    public boolean addFeature(MaterialByUnit feature){
+    public boolean addFeature(MaterialByUnit feature) {
         this.featuresList.add(feature);
         return true;
     }
 
 
-    public MaterialByUnit getFeature(int index){
-            return this.featuresList.get(index);
+    public MaterialByUnit getFeature(int index) {
+        return this.featuresList.get(index);
     }
 
 
-    public boolean removeFeature(int index){
-        if(index > 0 && index < this.featuresList.size()-1){
+    public boolean removeFeature(int index) {
+        if (index > 0 && index < this.featuresList.size() - 1) {
             this.featuresList.remove(index);
             return true;
+
         }
-        else{
+        else {
             return false;
         }
     }
 
-    public int getFeatureListSize(){
+    public int getFeatureListSize() {
         return this.featuresList.size();
     }
-
-    class InternalWall extends WallImpl{
-        private double height;
-        private double length;
-        private double width;
-        private MaterialByArea material;
-
-
-        public InternalWall(){
-
-        }
-
-     }
 }
--- src/test/java/edu/ithaca/Group6/InternalWallTest.java
+++ src/test/java/edu/ithaca/Group6/InternalWallTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
+
 public class InternalWallTest {
+    Wall testWall;
 
     public InternalWallTest(){
         this.testWall = new InternalWall();
