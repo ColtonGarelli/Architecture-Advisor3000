385bb1f1f3811befde44c9fe9ab9d5011f63ac23
benbjamin123 <ben.tgms@gmail.com>
2018-04-27 20:27:16 -0400

    Added a brace
    The first addWallFeature function was missing a curly brace, so the circleCI tests were still failing

--- src/main/java/edu/ithaca/Group6/BuildingImpl.java
+++ src/main/java/edu/ithaca/Group6/BuildingImpl.java
@@ -227,6 +227,7 @@ public class BuildingImpl implements Building{
 
     public void addWallFeature(int wallIdx, MaterialByUnit feature){
         this.walls[wallIdx].addFeature(feature);
+    }
 
         //For when features have position
     public void addWallFeature(int wallIdx, MaterialByUnit feature, double[] startPoint){
